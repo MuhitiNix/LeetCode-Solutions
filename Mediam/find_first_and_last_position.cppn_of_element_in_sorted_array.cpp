7e9b996a3de329d8d76cb9d3182a4141e195cd43
Mohaiminul Islam <76485709+MuhitiNix@users.noreply.github.com>
2025-10-26 01:28:17 +0600

    Added Solution for find_first_and_last_position.cppn_of_element_in_sorted_array

---
+++ Mediam/find_first_and_last_position.cppn_of_element_in_sorted_array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return {-1, -1};
+        int left = 0, right = n - 1;
+        int start = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (left < n && nums[left] == target) {
+            start = left;
+        }
+     
+        left = 0, right = n - 1;
+        int end = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (right >= 0 && nums[right] == target) {
+            end = right;
+        }
+        
+        return {start, end};
+    }
+};
